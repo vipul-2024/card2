8249a8d344391412711734f0c2b7d2829c1a69aa
vipul prajapati <vipulgithub020@gmail.com>
2025-03-21 14:04:55 +0000

    add color in front of card

--- components/Card_temp_2.jsx
+++ components/Card_temp_2.jsx
@@ -7,9 +7,12 @@ export default function Card_temp_1({ toggle, settoggle }) {
     < >
     <div className="flex gap-4 flex-wrap justify-center items-center md:flex-row gap-4 ">
   <div className="book">
-    <p>Hello</p>
+    <p>Chat-apk</p>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <a href="https://chat-app-ruddy-iota.vercel.app/">View</a>
+      </button>
     <div className="cover">
-      <p>Hover Me</p>
+      <p >Chat-apk</p>
     </div>
   </div>
   <div className="book">
